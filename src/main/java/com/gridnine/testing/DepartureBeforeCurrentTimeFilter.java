5fca421c1aff5997f54ef73292ebaae44610f74b
Albert Zaroyan <albertzaroyan@yandex.ru>
2023-08-16 15:31:03 +0300

    "add DepartureBeforeCurrentTimeFilter class"

--- src/main/java/com/gridnine/testing/DepartureBeforeCurrentTimeFilter.java
+++ src/main/java/com/gridnine/testing/DepartureBeforeCurrentTimeFilter.java
@@ -3,7 +3,7 @@ package com.gridnine.testing;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
-public class DepartureBeforeCurrentTimeFilter implements FlightFilter{
+public class DepartureBeforeCurrentTimeFilter implements FlightFilter {
     @Override
     public List<Flight> filterFlights(List<Flight> flights, Object... parameters) {
         LocalDateTime currentTime = LocalDateTime.now();
